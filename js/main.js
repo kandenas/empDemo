dcc213678a7f16b0a9675ec1b026b08f16596019
Kjetil Andenas <kjetil@symfoni.no>
2015-01-19 21:39:40 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -12,9 +19,23 @@ var app = {
             }
         });
     },
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Search</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+        var self = this;
+        this.store = new WebSqlStore(function() {
+            self.showAlert('Store initialized', 'Info');
+            self.renderHomeView();
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 

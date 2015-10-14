54a1042d5fd129dbb63399fc5284bc8b249b65f0
Kjetil Andenas <kjetil@symfoni.no>
2015-10-14 20:08:40 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -7,36 +7,13 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-    renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Search</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
 
     initialize: function() {
         var self = this;
         this.store = new WebSqlStore(function() {
-            self.showAlert('Store initialized', 'Info');
-            self.renderHomeView();
+    //        self.showAlert('Welcome to the contact application', 'Contacts');
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
